2543da496187d275185506c6413080ff03bba77e
nahidukil547 <nahidukil547@gmail.com>
2025-09-03 02:36:01 +0600

    Fix-Add: Reset, Forgor, Change Password Updated

--- basic_auth/urls.py
+++ basic_auth/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register_view, name='register'),
+    path('register/', views.register_view, name='register'),
     path('login/', views.login_view_username_pass, name='login'),
+    path("logout/", views.user_logout, name="logout"),
+    path("reset-password/", views.reset_pass, name="reset_password"),
+    path("change-password/", views.change_pass, name="change_password"),
+    path("forgot-password/", views.forgot_pass, name="forgot_password"),
+    path('', views.home, name='home'),
 ]
--- basic_auth/views.py
+++ basic_auth/views.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 from .forms import UserRegisterForm
 from django.contrib import messages
 from django.core.exceptions import ValidationError
-from django.contrib.auth import login as user_login, logout as user_logout, authenticate 
+from django.contrib.auth import login as user_login, logout , authenticate 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import update_session_auth_hash
 # Create your views here.
 
 def register_view(request):
@@ -59,11 +61,17 @@ def login_view_username_pass(request):
     if request.method=="POST":
         username=request.POST.get('username')
         password=request.POST.get('password')
+        
         user = authenticate(request, username=username, password=password)
+        
+        if user is None:
+            raise ValidationError("Invalid username and password")
+        print(username, password)
         if user is not None:
             user_login(request, user)
-            return redirect('register')  
+            return redirect('home')  
     return render(request,'login.html')
+
 
 """
 without Form get data using request.POST.get('....')
@@ -89,3 +97,68 @@ if we use form for login
 
     return render(request, "login.html", {"form": form})
 """
+
+@login_required
+def home(request):
+    return render(request,'home.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
+
+
+@login_required
+def reset_pass(request):
+    if request.method=="POST":
+        username= request.POST.get('username')
+        password=request.POST.get('password')
+        confirm_password=request.POST.get('confirm_password')
+        if password != confirm_password:
+            raise ValidationError("Passwords do not match")
+        user= User.objects.get(username=username)
+        user.set_password(password)
+        user.save()
+        return redirect('login')
+    return render(request,'reset-password.html')
+
+
+
+
+def change_pass(request):
+    if request.method =="POST":
+        user =request.user
+        old_password= request.POST.get('old_password')
+        new_password= request.POST.get('new_password')
+        confirm_new_password= request.POST.get('confirm_password')
+
+        if not user.check_password(old_password):
+            messages.error(request, "Old password is incorrect.")
+            return redirect('change_password')
+        
+        if new_password != confirm_new_password:
+            messages.error(request, "New password and confirm password do not match.")
+            return redirect('change_password')
+        
+        user.set_password(new_password)
+        user.save()
+        update_session_auth_hash(request, user)  
+        
+        return redirect('home')
+    return render(request, 'change-password.html')        
+
+
+def forgot_pass(request):
+    if request.method=="POST":
+        username= request.POST.get('username')
+        new_password=request.POST.get('new_password')
+        confirm_new_password=request.POST.get('confirm_password')
+
+        if new_password != confirm_new_password:
+            raise ValidationError("Passwords do not match")
+        
+        user= User.objects.get(username=username)
+        user.set_password(new_password)
+        user.save()
+        return redirect('login')
+    return render(request,'forgot-password.html')
